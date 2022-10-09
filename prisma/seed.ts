f8b08d4c461a6feaf392c1c34058d6d1d38c50b3
lvneto <lauro.neto.1995@gmail.com>
2022-10-09 11:06:58 -0300

    crud users and connection with prisma working

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  // create two dummy articles
+  const user1 = await prisma.user.create({
+    data: {
+      name: 'lvneto',
+    },
+  });
+
+  console.log({ user1 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
